3fc494addbea42d52579d2cce3e3a7fcd037c094
Lukasz Kowalski <lukasz.kowalski91@gmail.com>
2014-08-04 15:55:15 +0200

    Initial Commit

---
+++ ToDoApp2/ToDoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ToDoAppDelegate.h
+//  ToDoApp2
+//
+//  Created by Łukasz Kowalski on 8/4/14.
+//  Copyright (c) 2014 Lukasz Kowalski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToDoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoApp2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoApp2
+//
+//  Created by Łukasz Kowalski on 8/4/14.
+//  Copyright (c) 2014 Lukasz Kowalski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ToDoAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ToDoAppDelegate class]));
+    }
+}
